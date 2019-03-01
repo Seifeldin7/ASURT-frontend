829cb2045405d9a9022ba61603990a67d80df861
Ahmed Ebeid <a.ebeid@hotmail.com>
2019-03-02 00:31:25 +0200

    Intial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,60 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+
+//Directives
+
+
+// Services
+import { AuthenticationService } from './Services/Authentication/authentication.service';
+import { GeneralService } from './Services/General/general.service';
+import { ProfileService } from './Services/Profile/profile.service';
+
+
+// Routers
+import { AuthenticationRoutesModule } from './Routing/Authentication/authentication.service';
+import { GeneralRoutesModule } from './Routing/General/general.service';
+import { ProfileRoutesModule } from './Routing/Profile/profile.router';
+
+
+//AuthGuard
+
+
+//Pipes
+
+
+// Components
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './Components/authentication/authentication.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { GeneralComponent } from './Components/general/general.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    ProfileComponent,
+    GeneralComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthenticationRoutesModule,
+    GeneralRoutesModule,
+    ProfileRoutesModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: '**', redirectTo: 'login' }
+    ])
   ],
-  providers: [],
+  providers: [
+    AuthenticationService,
+    GeneralService,
+    ProfileService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
