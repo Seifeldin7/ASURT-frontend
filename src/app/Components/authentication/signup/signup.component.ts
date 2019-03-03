69d23c7da673d29ded3708c7943bb28ba1e19367
anssk <anss.khaled00@gmail.com>
2019-03-03 17:27:09 +0200

    edit signup component

--- src/app/Components/authentication/signup/signup.component.ts
+++ src/app/Components/authentication/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
 // import { AlertService} from '../../../Services/Authentication/alert.service';
 // import { UserService} from '../../../Services/Authentication/user.service';
@@ -11,9 +13,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
     registerForm: FormGroup;
+    
+    loading = false;
+    submitted = false;
 
     constructor(
-        private formBuilder: FormBuilder
+        private formBuilder: FormBuilder,
+        // private router: Router,
+        // private userService: UserService,
+        // private alertService: AlertService
         ) { }
 
     ngOnInit() {
@@ -29,5 +37,24 @@ export class SignupComponent implements OnInit {
     get f() { return this.registerForm.controls; }
 
     onSubmit() {
+        // this.submitted = true;
+
+        // // stop here if form is invalid
+        // if (this.registerForm.invalid) {
+        //     return;
+        // }
+
+        // this.loading = true;
+        // this.userService.register(this.registerForm.value)
+        //     .pipe(first())
+        //     .subscribe(
+        //         data => {
+        //             this.alertService.success('Registration successful', true);
+        //             this.router.navigate(['/login']);
+        //         },
+        //         error => {
+        //             this.alertService.error(error);
+        //             this.loading = false;
+        //         });
     }
 }
