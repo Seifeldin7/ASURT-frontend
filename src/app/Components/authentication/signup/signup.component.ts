3242bb2a9f895dcdc1b005b6b4c583b55ee95f72
anssk <anss.khaled00@gmail.com>
2019-03-06 05:42:00 +0200

    Modify Signup component
    Add validation and errors messages for each field
    
    Implement submit function to send request to backend

--- src/app/Components/authentication/signup/signup.component.ts
+++ src/app/Components/authentication/signup/signup.component.ts
@@ -1,38 +1,67 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
+
+import { AuthenticationService, passwordMatchValidator } from 'src/app/Services/Authentication/authentication.service';
 
 
 @Component({
     selector: 'app-signup',
     templateUrl: 'signup.component.html',
     styleUrls: ['./signup.component.css']
-
 })
 export class SignupComponent implements OnInit {
-    registerForm: FormGroup;
 
-    loading = false;
-    submitted = false;
+    /**
+     * HTML Elements
+     */
+    registerForm:FormGroup = this.formBuilder.group({
+        email: ['', [Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]],
+
+        password: this.formBuilder.group({
+            password1: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(16)]],
+            password2: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(16)]]
+        },{validators:[passwordMatchValidator]}),
+
+    });
 
     constructor(
         private formBuilder: FormBuilder,
+        private router: Router,
+        private authService: AuthenticationService
     ) { }
 
     ngOnInit() {
-        // this.registerForm = this.formBuilder.group({
-        //     firstName: ['', Validators.required],
-        //     lastName: ['', Validators.required],
-        //     username: ['', Validators.required],
-        //     password: ['', [Validators.required, Validators.minLength(6)]]
-        // });
+
+        this.authService.isLoggedIn().subscribe(
+            status => {
+                if(status){
+                    this.router.navigate(['/']);
+                }else{}
+            }
+        );
     }
 
     // convenience getter for easy access to form fields
-    // get f() { return this.registerForm.controls; }
+    get f() { return this.registerForm.controls; }
 
     onSubmit() {
-
+        this.authService.signup({
+            email: this.registerForm.value.email,
+            password: this.registerForm.value.password.password1,
+        }).subscribe(
+            (response:any) => {
+                if(response.token){
+                    this.authService.storeToken(response.token);
+                    this.router.navigate(['/']);
+                    console.log('signed up');
+                }else{
+                    console.log('register form response error');
+                }
+            },
+            (err)=>{
+                console.log('resister form err submit');
+            }
+        )
     }
 }
