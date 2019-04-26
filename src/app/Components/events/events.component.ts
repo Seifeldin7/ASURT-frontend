93c13535b95c99e0c228e6ff8865ef8a011e232c
seif eldin Mohamed <s.seif26@yahoo.com>
2019-04-26 22:56:16 +0200

    added routing

--- src/app/Components/events/events.component.ts
+++ src/app/Components/events/events.component.ts
@@ -4,6 +4,9 @@ import Swal from 'sweetalert2';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Evnt } from '../../Models/event.model';
 
+declare var jquery: any;
+declare var $: any;
+
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
@@ -24,11 +27,36 @@ export class EventsComponent implements OnInit {
       this.events = events;
       this.eventService.setEvents(this.events);
     },error => {
+      console.log(error)
       Swal.fire({
+        
         title: 'Something went wrong!',
+
       })
     }
   );
+  $('#recipeCarousel').carousel({
+    interval: 1500
+  })
+  $('.carousel .carousel-item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<4;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
+    $('.carousel').carousel({
+      pause: "false"
+    });
   // this.events = [new Evnt(1,"formula","awesome event","22-2-123",[{id:1,image:"232"}],"dont know",true),
   // new Evnt(2,"rov","awesome event","22-2-143",[{id:1,image:"232"}],"dont know2",true),new Evnt(2,"rov","awesome event","22-2-143",[{id:1,image:"232"}],"dont know2",true),
   // new Evnt(2,"rov","awesome event","22-2-143",[{id:1,image:"232"}],"dont know2",true)];
