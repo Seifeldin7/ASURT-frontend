31ac0579a5378f81ae7d38acfa8613293db1ae1c
seif eldin Mohamed <s.seif26@yahoo.com>
2019-05-02 18:29:28 +0200

    changed appearance

--- src/app/Components/events/event-detail/event-detail.component.ts
+++ src/app/Components/events/event-detail/event-detail.component.ts
@@ -33,7 +33,7 @@ export class EventDetailComponent implements OnInit {
     
   });
   }
-  back(){
-    this.router.navigate(['../events']);
-  }
+  // back(){
+  //   this.router.navigate(['../events']);
+  // }
 }
