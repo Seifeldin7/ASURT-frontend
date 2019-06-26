ca2dcc5e368d581af6ff1f2ff3980922c94d42d2
Peter Nabil <peter@github.com>
2019-06-26 14:17:15 +0300

    final newsfeed isa

--- src/app/Components/news-feed/post/post.component.ts
+++ src/app/Components/news-feed/post/post.component.ts
@@ -1,21 +1,69 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener, ElementRef } from '@angular/core';
 import { Post } from 'src/app/Models/post.model';
 import { DomSanitizer } from '@angular/platform-browser';
 import { img } from 'src/app/Models/img.model';
+//import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
+  // animations: [
+  //   trigger('scrollAnimation', [
+  //     state('show', style({
+  //       opacity: 1,
+  //       transform: "translateX(0)"
+  //     })),
+  //     state('hide',   style({
+  //       opacity: 0,
+  //       transform: "translateX(-90%)"
+  //     })),
+  //     transition('show => hide', animate('700ms ease-out')),
+  //     transition('hide => show', animate('700ms ease-in'))
+  //   ])
+  // ]
 })
 export class PostComponent implements OnInit {
   image:img['image'];
   len:number;
   imgRight=true;
+  state = 'show'
+  small :boolean;
+
 
   @Input() post:Post;
   @Input() index:number;
-  constructor(private sanitizer:DomSanitizer) { }
+
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event){
+    if (window.innerWidth < 760) {
+      this.small = true;
+    } else {
+      this.small = false;
+    }
+
+  }
+  constructor(private sanitizer:DomSanitizer,public el:ElementRef) {
+
+    if (window.innerWidth < 760) {
+      this.small = true;
+    } else {
+      this.small = false;
+    }
+  }
+  //@HostListener('window:scroll', ['$event'])
+  // checkScroll() {
+  //   const componentPosition = this.el.nativeElement.offsetTop
+  //   const scrollPosition = window.pageYOffset+550
+  //
+  //   if (scrollPosition <= componentPosition) {
+  //     this.state = 'hide'
+  //   } else {
+  //     this.state = 'show'
+  //   }
+  //
+  // }
 
   ngOnInit() {
     if(this.index%2==0){
@@ -26,7 +74,7 @@ export class PostComponent implements OnInit {
       //console.log(this.post.image[this.post.image.length-1]);
       this.image=this.post.image[this.len]['image'];
     }
-   
+
   }
   embedUrl(post){
   return  this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/"+post.video);
--- src/app/Services/Authentication/authentication.service.ts
+++ src/app/Services/Authentication/authentication.service.ts
@@ -84,7 +84,7 @@ export class AuthenticationService {
     /**
      * Check if user logged in and verify token
      *
-     * 
+     *
      * @returns Subject<boolean> Of loggedin status
      */
     let token = localStorage.getItem('token') ? localStorage.getItem('token') : null;
@@ -92,7 +92,7 @@ export class AuthenticationService {
 
     if(token == null){
       setTimeout(() => {
-        this.verifyLoggedIn.next(false);        
+        this.verifyLoggedIn.next(false);
       });
     }else {
       this.tokenVerify(token).
@@ -173,7 +173,7 @@ export function passwordMatchValidator(ac: AbstractControl) {
 export class JwtInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let token = null;
-    
+
     if(localStorage.getItem('token') === null){
       token = null;
     }
@@ -182,8 +182,8 @@ export class JwtInterceptor implements HttpInterceptor {
       token = JSON.parse(localStorage.getItem('token'));
       // token = localStorage.getItem('token');
     }
-    
-    
+
+
     if (token) {
       request = request.clone({
         setHeaders: {
@@ -198,8 +198,8 @@ export class JwtInterceptor implements HttpInterceptor {
 
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
-  baseUrl = 'http://127.0.0.1:8000/';
-   //baseUrl = 'http://localhost:3000/';
+  //baseUrl = 'http://127.0.0.1:8000/';
+  baseUrl = 'http://localhost:3000/';
   // baseUrl ='https://domain-name.com/';
   constructor() { }
 
